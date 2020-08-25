e10f7ff51f94aa81e560e96894a15bb66976d252
Elle <elleobrien11@gmail.com>
2020-08-25 14:12:09 -0700

    update data generating function

--- get_data.py
+++ get_data.py
@@ -5,12 +5,15 @@ import numpy as np
 
 # If there's no dataset in the project directory, create a reasonably large one. 
 # If it exists, append some new observations. 
-if os.path.isfile("data.csv"):
+if os.path.isfile("data/features.csv"):
     n = 1
 else:
-    n = 50
+    os.mkdir("data")
+    n = 10
 
 for i in range(0,n):    
     X, y = make_regression(10000,n_features = 10)
     df = pd.DataFrame(X)
-    df.to_csv("data.csv",mode='a')
+    df.to_csv("data/features.csv",mode="a", index=False)
+    labels = pd.DataFrame(y)
+    labels.to_csv("data/labels.csv",mode="a",index=False)
